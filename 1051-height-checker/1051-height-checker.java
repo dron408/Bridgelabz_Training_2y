3a4415b295d5d8dd1b6c875db5a3ee719958a0d9
dron408 <dronsharma123123@gmail.com>
2026-08-19 12:17:18 +0530

    Time: 4 ms (79.11%), Space: 43.4 MB (45.57%) - LeetHub

---
+++ 1051-height-checker/1051-height-checker.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int heightChecker(int[] heights) {
+        int[] arr = new int[heights.length];
+        for(int i=0;i<heights.length;i++){
+            arr[i] = heights[i];
+        }
+
+        //  for(int i=0;i<heights.length;i++){
+        //     for(int j=i+1;j<heights.length;j++){
+        //         if(heights[j]>heights[i]){
+        //             int temp = heights[i];
+        //             heights[i] = heights[j];
+        //             heights[j] = temp;
+        //         }
+        //     }
+        // }
+        Arrays.sort(heights);
+        int count = 0;
+
+        for(int i=0;i<heights.length;i++){
+                if(arr[i]!=heights[i]){
+                    count++;
+                }
+        }
+        return count;
+        
+    }
+}
